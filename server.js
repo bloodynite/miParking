6bfda4ac61d4301688a9e4f2139c21fe2a8af7db
Francisco Iturriaga <francisco382@gmail.com>
2021-11-23 18:27:25 -0300

    [FIX] server.js path

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/miParking'));
+app.use(express.static('./www'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/miParking/'}),
+    res.sendFile('index.html', {root: 'www/'}),
 );
 
 // Start the app by listening on the default Heroku port
